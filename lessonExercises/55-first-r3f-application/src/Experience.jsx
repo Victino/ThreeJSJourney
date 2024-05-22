297f76b42366bf0b12d49d9be00a6fd18f6c77d0
Vito Jak <93348449+Victino@users.noreply.github.com>
2024-05-23 00:12:33 +0200

    finished 55 + 56: Drei
    Drei seems very useful with even being able to move objects around in the seen and making some types of objects easier.... does take extra learning but seems worth

--- lessonExercises/55-first-r3f-application/src/Experience.jsx
+++ lessonExercises/55-first-r3f-application/src/Experience.jsx
@@ -1,44 +1,51 @@
 import { useThree, extend, useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
-import CustomObject from './CustomObject.jsx'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import CustomObject from './CustomObject.jsx'
+
 extend({ OrbitControls })
 
 export default function Experience()
 {
-    useFrame((state, delta) =>
-    {
-        cubeRef.current.rotation.y += delta
-        groupRef.current.rotation.y += delta
-    })
+    const { camera, gl } = useThree()
 
     const cubeRef = useRef()
     const groupRef = useRef()
-    const three = useThree()
-    const { camera, gl } = useThree()
+
+    useFrame((state, delta) =>
+    {
+        // const angle = state.clock.elapsedTime
+        // state.camera.position.x = Math.sin(angle) * 8
+        // state.camera.position.z = Math.cos(angle) * 8
+        // state.camera.lookAt(0, 0, 0)
+
+        cubeRef.current.rotation.y += delta
+        // groupRef.current.rotation.y += delta
+    })
 
     return <>
-    <orbitControls args={ [ camera, gl.domElement ] } />
-    <directionalLight />
-    <ambientLight intensity={ 1.5 } />
-    
-    <CustomObject />
-    
-<group ref={ groupRef }>
-    <mesh position-x={ - 2 }>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-    </mesh>
+        <orbitControls args={ [ camera, gl.domElement ] } />
 
-    <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-    </mesh>
-</group>
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <ambientLight intensity={ 1.5 } />
 
-<mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-    <planeGeometry />
-    <meshStandardMaterial color="greenyellow" />
-</mesh>
+        <group ref={ groupRef }>
+            <mesh position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+
+            <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
+        </group>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+        <CustomObject />
     </>
 }
